6d9eed19b0ed5743268a228788b04db48c49bbd5
Chang Winson <102290205+chang-winson@users.noreply.github.com>
2022-10-12 19:48:07 +0800

    Add files via upload

---
+++ cmds/ping.js
@@ -0,0 +1,8 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('ping').setDescription('得知機器人當前的延遲數據'),
+	async execute(interaction) {
+		await interaction.reply(`🏠╎DiscordAPI延遲：${interaction.client.ws.ping}ms\n🤖╎機器人延遲：${Date.now() - interaction.createdTimestamp}ms`);
+	}
+};
