1bb41adc9e4ff42d3f3aa8305de2ddd5164ebf86
nk980113 <50331539+nk980113@users.noreply.github.com>
2022-10-15 20:52:10 +0800

    style

--- evnts/interaction.js
+++ evnts/interaction.js
@@ -1,14 +1,19 @@
+const { InteractionType } = require('discord-api-types/v10');
+
 module.exports = {
-	name: 'interactionCreate',
-	async execute(interaction) {
-		if(!interaction.isChatInputCommand()) return;
-		const cmd = interaction.client.commands.get(interaction.commandName);
-		if (!cmd) return;
-		try {
-			await cmd.execute(interaction);
-		} catch(err) {
-			if(err) console.error(err);
-			await interaction.reply({ content: '機器人發生錯誤，請回報開發人員或稍後再試', ephemeral: true });
-		};
-	}
+    name: 'interactionCreate',
+    async execute(interaction) {
+        switch (interaction.type) {
+            case InteractionType.ApplicationCommand: {
+                const cmd = interaction.client.commands.get(interaction.commandName);
+                if (!cmd) return;
+                try {
+                    await cmd.execute(interaction);
+                } catch (err) {
+                    if (err) console.error(err);
+                    await interaction.reply({ content: '機器人發生錯誤，請回報開發人員或稍後再試', ephemeral: true });
+                };
+            }
+        }
+    }
 };
